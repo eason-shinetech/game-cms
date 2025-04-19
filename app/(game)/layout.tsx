245e6fd84e6de238bd6b2e6ae4787c11f7483a8a
eason-shinetech <zhaost@shinetechsoftware.com>
2025-04-19 23:05:33 +0800

    test ads

--- app/(game)/layout.tsx
+++ app/(game)/layout.tsx
@@ -1,6 +1,5 @@
 import ToasterProvider from "@/components/providers/toaster-provider";
 import GameHeader from "./_components/header";
-import Script from "next/script";
 
 export default function GameLayout({
   children,
@@ -9,11 +8,6 @@ export default function GameLayout({
 }) {
   return (
     <div>
-      <Script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9748776301585000"
-        crossOrigin="anonymous"
-      />
       <div className="h-[60px] fixed inset-y-0 w-full z-50">
         <GameHeader />
       </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Logo from "../components/commons/logo";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -42,7 +43,7 @@ export const metadata: Metadata = {
   ],
   metadataBase: new URL("https://freegame.funnyplayers.com"),
   alternates: {
-    canonical: "/"
+    canonical: "/",
   },
   other: {
     "google-adsense-account": "ca-pub-9748776301585000",
@@ -56,6 +57,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9748776301585000"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
