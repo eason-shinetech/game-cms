233d984387df4e631fe00cec23a679610993594e
eason-shinetech <zhaost@shinetechsoftware.com>
2025-04-19 17:43:13 +0800

    add log

--- middleware.ts
+++ middleware.ts
@@ -4,19 +4,19 @@ import { authConfig } from "./auth.config";
 const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
-    const { nextUrl } = req;
-    // req.auth is provided by Auth.js
-    console.log('req.auth.user: ', req.auth?.user);
+  const { nextUrl } = req;
+  // req.auth is provided by Auth.js
+  console.log("req.auth.user: ", req.auth?.user, nextUrl);
+  console.log("req.auth.user: ", nextUrl.pathname, nextUrl);
+  // set isAuthenticated to true if req.auth is a truthy value. otherwise set to false.
+  const isAuthenticated = !!req.auth;
 
-    // set isAuthenticated to true if req.auth is a truthy value. otherwise set to false.
-    const isAuthenticated = !!req.auth;
+  // use boolean value to determine if the requested route is a protected route
+  const isProtectedRoute = ["/admin"].includes(nextUrl.pathname);
 
-    // use boolean value to determine if the requested route is a protected route
-    const isProtectedRoute = ['/admin'].includes(nextUrl.pathname);
-
-    // redirect to signin if route is a protected route and user is not authenticated
-    if (isProtectedRoute && !isAuthenticated)
-        return Response.redirect(new URL('/login', nextUrl));
+  // redirect to signin if route is a protected route and user is not authenticated
+  if (isProtectedRoute && !isAuthenticated)
+    return Response.redirect(new URL("/login", process.env.NEXTAUTH_URL));
 });
 
 export const config = {
