9d5e36a66888c30982642f35159eadde79c6412a
eason-shinetech <zhaost@shinetechsoftware.com>
2025-05-13 17:56:19 +0800

    test

--- middleware.ts
+++ middleware.ts
@@ -7,6 +7,18 @@ const { auth } = NextAuth(authConfig);
 export default auth((req) => {
   const { nextUrl } = req;
 
+  console.log("nextUrl", nextUrl.pathname);
+
+  if (nextUrl.pathname === "/cdn-cgi/rum") {
+    console.log("Ignore /cdn-cgi/rum");
+    return new Response(null, {
+      status: 204,
+      headers: {
+        "CF-RAY": req.headers.get("CF-RAY") || "fallback_ray",
+        "CF-Connecting-IP": req.headers.get("X-Forwarded-For") || "",
+      },
+    });
+  }
   // set isAuthenticated to true if req.auth is a truthy value. otherwise set to false.
   const isAuthenticated = !!req.auth;
 
